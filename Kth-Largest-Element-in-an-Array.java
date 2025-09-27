f48d3fa59458919833f44d870b5bbfa03243babf
khushi1825 <myselfkhushi2005@gmail.com>
2025-09-27 21:38:23 +0530

    [215] [Time Beats: 69.82%] [Memory Beats: 79.13%] - LeetPush

---
+++ Kth-Largest-Element-in-an-Array.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> q=new PriorityQueue<>();
+        for (int i=0;i<k;i++){
+            q.add(nums[i]);
+        }
+        
+        for (int i=k;i<nums.length;i++){
+            if (nums[i]>q.peek()){
+                q.poll();
+                q.offer(nums[i]);
+            }
+        }
+        
+        return q.peek();
+    }
+}
