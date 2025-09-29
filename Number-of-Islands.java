33af3780fb031a657ce825bb148476b49d592dab
khushi1825 <myselfkhushi2005@gmail.com>
2025-09-29 11:54:44 +0530

    [200] [Time Beats: 84.37%] [Memory Beats: 89.49%] - LeetPush

--- Number-of-Islands.java
+++ Number-of-Islands.java
@@ -1,52 +1,30 @@
 class Solution {
-
-    static boolean isValid(int n_r, int n_c, int rows, int cols, boolean[][] visited, char[][] grid) {
-        return (n_r >= 0 && n_r < rows && n_c >= 0 && n_c < cols &&
-                !visited[n_r][n_c] && grid[n_r][n_c] == '1');
-    }
-
-    static void bfs_mat(char[][] grid, int sr, int sc, int rows, int cols, boolean[][] visited) {
-        Queue<int[]> q = new LinkedList<>();
-        visited[sr][sc] = true;
-        q.add(new int[]{sr, sc});
-        int dr[] = {0, 1, 0, -1};
-        int dc[] = {1, 0, -1, 0};
-
-        while (!q.isEmpty()) {
-            int curr[] = q.poll();
-            int curr_row = curr[0];
-            int curr_col = curr[1];
-
-            for (int i = 0; i < dr.length; i++) {
-                int nbr_row = curr_row + dr[i];
-                int nbr_col = curr_col + dc[i];
-
-                if (isValid(nbr_row, nbr_col, rows, cols, visited, grid)) {
-                    visited[nbr_row][nbr_col] = true;
-                    q.add(new int[]{nbr_row, nbr_col});
-                }
-            }
-        }
-    }
-
-    static int bfs_all(char[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        boolean[][] visited = new boolean[m][n];
-        int count = 0;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (!visited[i][j] && grid[i][j] == '1') {
-                    bfs_mat(grid, i, j, m, n, visited);
-                    count++;
-                }
-            }
-        }
-        return count;
-    }
-
     public int numIslands(char[][] grid) {
-        return bfs_all(grid);
+        int c=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]=='1'){
+                    c++;
+                    clear(grid,i,j);
+                }
+            }
+        }
+
+        return c;
+
+    }
+
+    void clear(char grid [][],int r,int c){
+
+        if(r<0 || r>=grid.length || c<0 || c>=grid[0].length || grid[r][c]=='0'){
+            return;
+        }
+        
+        grid[r][c]='0';
+        clear(grid,r+1,c);
+        clear(grid,r-1,c);
+        clear(grid,r,c+1);
+        clear(grid,r,c-1);
+        
     }
 }
