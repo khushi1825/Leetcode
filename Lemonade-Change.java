6a23f44ed9ce11ec38ec6a8f425ac45d7d4102f2
khushi1825 <myselfkhushi2005@gmail.com>
2025-10-01 23:04:04 +0530

    [860] [Time Beats: 99.38%] [Memory Beats: 54.58%] - LeetPush

---
+++ Lemonade-Change.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five=0;
+        int ten=0;
+
+        for (int x:bills){
+            if(x==5){
+                five++;
+            } 
+            else if(x==10){
+                if (five>0){
+                    five--;
+                    ten++;
+                } 
+                else{
+                    return false;
+                }
+            } 
+            else {
+                if (five>0 && ten>0){
+                    five--;
+                    ten--;
+                } 
+                else if(five>2){
+                    five-=3;
+                } 
+                else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
