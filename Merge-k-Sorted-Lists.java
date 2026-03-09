e6337f5d5e2de48be639c5083f850f20fb9e5ccf
khushi1825 <myselfkhushi2005@gmail.com>
2026-03-09 16:30:26 +0530

    [23] [Time Beats: 80.58%] [Memory Beats: 43.31%] - LeetPush

--- Merge-k-Sorted-Lists.java
+++ Merge-k-Sorted-Lists.java
@@ -8,22 +8,24 @@
 8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 9 * }
 10 */
-11class Solution {
-12    public ListNode mergeKLists(ListNode[] lists) {
-13        List<Integer> res=new ArrayList<>();
+11class Solution{
+12    public ListNode mergeKLists(ListNode[] lists){
+13        PriorityQueue<ListNode> q=new PriorityQueue<>((a,b)->a.val-b.val);//sorting in adcending order insertion time of pq logn
 14        for(ListNode h:lists){
-15            while(h!=null){
-16                res.add(h.val);
-17                h=h.next;
-18            }
-19        }
-20        Collections.sort(res);
-21        ListNode dummy=new ListNode(-1);//tail
-22        ListNode curr=dummy;
-23        for(int val:res){
-24            curr.next=new ListNode(val);
-25            curr=curr.next;
-26        }
-27        return dummy.next;   
-28    }
-29}
+15            if(h!=null){
+16                q.add(h);
+17            }
+18        }
+19        ListNode dummy=new ListNode();
+20        ListNode tail=dummy;
+21        while(!q.isEmpty()){
+22            ListNode curr=q.poll();
+23            tail.next=curr;
+24            tail=tail.next;
+25            if(curr.next!=null){
+26                q.add(curr.next);
+27            }
+28        }
+29        return dummy.next;
+30    }
+31}
