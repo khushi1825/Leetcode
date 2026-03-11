1dbec89d9dd34c90ddf9c3a2f7776ae5ec94323f
khushi1825 <myselfkhushi2005@gmail.com>
2026-03-11 11:11:29 +0530

    [103] [Time Beats: 78.51%] [Memory Beats: 98.41%] - LeetPush

--- Binary-Tree-Zigzag-Level-Order-Traversal.java
+++ Binary-Tree-Zigzag-Level-Order-Traversal.java
@@ -1,52 +1,48 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<>();
-        if (root == null){ 
-            return result;
-        }
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        boolean leftToRight = true;
-
-        while (!q.isEmpty()) {
-            int size = q.size();
-            List<Integer> level = new ArrayList<>();
-
-            for (int i = 0; i < size; i++) {
-                TreeNode node = q.poll();
-
-                if (leftToRight) {
-                    level.add(node.val);
-                } else {
-                    level.add(0, node.val); 
-                }
-
-                if (node.left != null){ 
-                    q.add(node.left);
-                }
-                if (node.right != null){
-                    q.add(node.right);
-                } 
-            }
-
-            result.add(level);
-            leftToRight = !leftToRight; 
-        }
-        return result;
-    }
-}
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+18        List<List<Integer>> res=new ArrayList<>();
+19        Queue<TreeNode> q=new LinkedList<>();
+20        if(root==null){
+21            return res;
+22        }
+23        q.add(root);
+24        boolean LeftToRight=true;
+25        while(!q.isEmpty()){
+26            int size=q.size();
+27            List<Integer> level=new ArrayList<>();
+28            for(int i=0;i<size;i++){
+29                TreeNode n=q.poll();
+30                if(LeftToRight){
+31                    level.add(n.val);
+32                }
+33                if(!LeftToRight){
+34                    level.add(0,n.val);
+35                }
+36                if(n.left!=null){
+37                    q.add(n.left);
+38                }
+39                if(n.right!=null){
+40                    q.add(n.right);
+41                }
+42            }
+43            res.add(level);
+44            LeftToRight=!LeftToRight;
+45        }
+46        return res;
+47    }
+48}
