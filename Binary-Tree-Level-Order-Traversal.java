2751931437aefa477bae75d4848882f0a30df48a
khushi1825 <myselfkhushi2005@gmail.com>
2026-03-11 10:16:57 +0530

    [102] [Time Beats: 95.12%] [Memory Beats: 37.52%] - LeetPush

--- Binary-Tree-Level-Order-Traversal.java
+++ Binary-Tree-Level-Order-Traversal.java
@@ -1,49 +1,41 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> list=new ArrayList<>();
-        if(root==null){
-            return list;
-        }
-        Queue<TreeNode> q=new LinkedList<>();
-        q.add(root);
-
-        
-        
-        while(!q.isEmpty()){
-            int len=q.size();
-            List<Integer> level=new ArrayList<>();
-    
-            for(int i=1;i<=len;i++){
-                TreeNode n=q.poll();
-                level.add(n.val);
-                if(n.left!=null){
-                    q.add(n.left);
-                }
-            
-                if(n.right!=null){
-                    q.add(n.right);
-                }
-            }
-
-            list.add(level);
-            
-        }
-
-        return list;
-    }
-}
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public List<List<Integer>> levelOrder(TreeNode root) {
+18        List<List<Integer>> res=new ArrayList<>();
+19        Queue<TreeNode> q=new LinkedList<>();
+20        if(root==null){
+21            return res;
+22        }
+23        q.add(root);
+24        while(!q.isEmpty()){
+25            int size=q.size();
+26            List<Integer> level=new ArrayList<>();
+27            for(int i=0;i<size;i++){
+28                TreeNode n=q.poll();
+29                level.add(n.val);
+30                if(n.left!=null){
+31                    q.add(n.left);
+32                }
+33                if(n.right!=null){
+34                    q.add(n.right);
+35                }
+36            }
+37            res.add(level);
+38        }
+39        return res;
+40    }
+41}
