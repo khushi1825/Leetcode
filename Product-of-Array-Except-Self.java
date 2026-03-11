355e309c8b3906185724541af944e8959b8df517
khushi1825 <myselfkhushi2005@gmail.com>
2026-03-11 15:26:22 +0530

    [238] [Time Beats: 26.45%] [Memory Beats: 95.82%] - LeetPush

--- Product-of-Array-Except-Self.java
+++ Product-of-Array-Except-Self.java
@@ -1,23 +1,20 @@
-class Solution {
-    public int[] productExceptSelf(int[] nums) {
-        int n=nums.length;
-        int [] left=new int[n];
-
-        left[0]=1;
-        for(int i=1;i<n;i++){
-            left[i]=left[i-1]*nums[i-1];
-        }
-
-        int [] right=new int[n];
-        right[n-1]=1;
-        for(int i=n-2;i>=0;i--){
-            right[i]=right[i+1]*nums[i+1];
-        }
-
-        for(int j=0;j<n;j++){
-            left[j]=left[j]*right[j];
-        }
-
-        return left;
-    }
-}
+1class Solution {
+2    public int[] productExceptSelf(int[] arr) {
+3        int n=arr.length;
+4        int [] left=new int[n];
+5        left[0]=1;
+6        for(int i=1;i<n;i++){
+7            left[i]=left[i-1]*arr[i-1];
+8        }
+9        int right[]=new int[n];
+10        right[n-1]=1;
+11        for(int i=n-2;i>=0;i--){
+12            right[i]=right[i+1]*arr[i+1];
+13        }
+14
+15        for(int i=0;i<n;i++){
+16            left[i]=left[i]*right[i];
+17        }
+18        return left;
+19    }
+20}
