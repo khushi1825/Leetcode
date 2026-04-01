93c2ca8d1b8962a5f77bbe09d1f3915c29b951c5
khushi1825 <myselfkhushi2005@gmail.com>
2026-04-01 16:50:07 +0530

    [450] [Time Beats: 100.00%] [Memory Beats: 46.51%] - LeetPush

--- Delete-Node-in-a-BST.java
+++ Delete-Node-in-a-BST.java
@@ -1,48 +1,44 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public TreeNode deleteNode(TreeNode root, int key) {
-        if (root == null) 
-            return null;
-
-        if (key < root.val) {
-            root.left = deleteNode(root.left, key);
-
-        } else if (key > root.val) {
-            root.right = deleteNode(root.right, key);
-
-        } else {
-            
-            if (root.left == null) 
-                return root.right;
-
-            if (root.right == null) 
-                return root.left;
-
-            TreeNode successor = findMin(root.right);
-            root.val = successor.val;
-            root.right = deleteNode(root.right, successor.val);
-        }
-        return root;
-    }
-
-   public TreeNode findMin(TreeNode node) {
-        while (node.left != null) {
-            node = node.left;
-        }
-        return node;
-    }
-}
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public TreeNode deleteNode(TreeNode root, int key) {
+18        if(root==null) return null;
+19        if(root.val<key){
+20            root.right=deleteNode(root.right,key);
+21        }
+22        else if(root.val>key){
+23            root.left=deleteNode(root.left,key);
+24        }
+25        else{
+26            if(root.left==null) return root.right;
+27            else if(root.right==null) return root.left;
+28            else{
+29                int min=min(root.right);
+30                root.right=deleteNode(root.right,min);
+31                root.val=min;
+32            }
+33        }
+34        return root;
+35    }
+36
+37    public static int min(TreeNode root){
+38        if(root==null){
+39           return Integer.MAX_VALUE;
+40        }
+41        int left=min(root.left);
+42        return Math.min(left,root.val);
+43    }
+44}
