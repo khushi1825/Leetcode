2e2f83519adbb2b0db732a36b83326a679f13138
khushi1825 <myselfkhushi2005@gmail.com>
2026-07-14 22:08:07 +0530

    [134] [Time Beats: 7.90%] [Memory Beats: 25.29%] - LeetPush

--- Leetcode/134-Gas-Station.java
+++ Leetcode/134-Gas-Station.java
@@ -4,25 +4,20 @@ class Solution {
         int n=gas.length;
         int totalG=0;
         int totalC=0;
-        for(int val:gas){
-            totalG+=val;
-        }
-        for(int val:cost){
-            totalC+=val;
-        }
-        if(totalG < totalC){ // solution does not exist for this
-            return -1;
-        }
         int start=0;
         int currG=0;
         for(int i=0;i<n;i++){
+            totalG+=gas[i];
+            totalC+=cost[i];
             currG+=(gas[i]-cost[i]);
             if(currG<0){
                 start=i+1; // choose next index as prev is eliminated
                 currG=0;
             }
         }
+        if(totalG < totalC){ // solution does not exist for this
+            return -1;
+        }
         return start;
-        // more optimise in next
     }
 }
