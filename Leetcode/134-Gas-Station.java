582ff213c49be695dad32c76ad3b632e3e949331
khushi1825 <myselfkhushi2005@gmail.com>
2026-07-14 22:06:22 +0530

    [134] [Time Beats: 99.25%] [Memory Beats: 83.06%] - LeetPush

--- Leetcode/134-Gas-Station.java
+++ Leetcode/134-Gas-Station.java
@@ -1,21 +1,28 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
+        //optimise
         int n=gas.length;
+        int totalG=0;
+        int totalC=0;
+        for(int val:gas){
+            totalG+=val;
+        }
+        for(int val:cost){
+            totalC+=val;
+        }
+        if(totalG < totalC){ // solution does not exist for this
+            return -1;
+        }
+        int start=0;
+        int currG=0;
         for(int i=0;i<n;i++){
-            int fuel=0;
-            boolean possible=true;
-            for(int j=0;j<n;j++){
-                int idx=(i+j)%n;// for circular traversal
-                fuel=fuel+gas[idx]-cost[idx];
-                if(fuel<0){
-                    possible=false;
-                    break;
-                }
-            }
-            if(possible){
-                return i;
+            currG+=(gas[i]-cost[i]);
+            if(currG<0){
+                start=i+1; // choose next index as prev is eliminated
+                currG=0;
             }
         }
-        return -1;
+        return start;
+        // more optimise in next
     }
 }
